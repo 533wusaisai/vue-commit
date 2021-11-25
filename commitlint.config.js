0d5b752bb6b024f9dee0ae25a76bb2bd1a31ad17
wusaisai <wusaisai335@163.com>
2021-11-25 21:13:12 +0800

    feat: 检验进度添加

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-    extends: [
-        '@commitlint/config-conventional'
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"],
     ],
-    rules: {
-        'type-enum': [
-            2, 'always', [
-                'feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert'
-            ]
-        ],
-        'subject-full-stop': [0, 'never'],
-        'subject-case': [0, 'never']
-    }
-}
+    "subject-full-stop": [0, "never"],
+    "subject-case": [0, "never"],
+  },
+};
